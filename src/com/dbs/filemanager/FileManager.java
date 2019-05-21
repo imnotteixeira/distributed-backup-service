4be2d0ba7c10d830ecf8973264fba31770aa1f8e
Mariana Aguiar <marianabarbosaaguiar@gmail.com>
2019-05-21 18:07:34 +0100

    method readFromFile

--- src/com/dbs/filemanager/FileManager.java
+++ src/com/dbs/filemanager/FileManager.java
@@ -12,7 +12,28 @@ import static java.nio.file.StandardOpenOption.*;
 
 public class FileManager {
 
-    public static void readFromFile() {
+    public static int readFromFile(String filePath, ByteBuffer data, long filePosition) {
+
+        Path path = Paths.get(filePath);
+        AsynchronousFileChannel fileChannel;
+        int nBytes;
+
+        try {
+            fileChannel = AsynchronousFileChannel.open(path, READ);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return -1;
+        }
+
+        Future<Integer> result = fileChannel.read(data, filePosition);
+
+        try {
+            nBytes = result.get();
+            return nBytes;
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            return -1;
+        }
 
     }
 
@@ -41,8 +62,12 @@ public class FileManager {
     }
 
     public static void main(String[] args) {
-        ByteBuffer data = ByteBuffer.wrap("cenas".getBytes());
-        System.out.println(FileManager.writeToFile("file.txt", data, 0));
+        ByteBuffer data = ByteBuffer.wrap("cenascenascenas".getBytes());
+        System.out.println(FileManager.writeToFile("out.txt", data, 0));
+
+        ByteBuffer data1 = ByteBuffer.allocate(1024);
+        System.out.println(FileManager.readFromFile("out.txt", data1, 0));
+        System.out.println(new String(data1.array()).trim());
     }
 
 }
