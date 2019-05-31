3b9e3b7d7e62624d7dfefcf85ec7e4945c4bbb69
Henrique Lima <henriquelim@hotmail.com>
2019-05-31 16:40:50 +0100

    Updated testApp to support arguments for backup and delete

--- src/TestApp.java
+++ src/TestApp.java
@@ -15,19 +15,27 @@ public class TestApp {
             BackupService peer = TestApp.lookup(args[0]);
             switch (args[1]) {
                 case "BACKUP":
-                    System.out.println(peer.backup("asd.asd"));
+                    if(args.length != 3){
+                        System.out.println("No file was provided. Usage: <PeerAP> BACKUP <fileName>");
+                    }else {
+                        System.out.println(peer.backup(args[2]));
+                    }
                     break;
                 case "RESTORE":
                     System.out.println(peer.restore("file"));
                     break;
                 case "DELETE":
-                    System.out.println(peer.delete("file"));
+                    if(args.length != 3){
+                        System.out.println("No file was provided. Usage: <PeerAP> DELETE <fileName>");
+                    }else {
+                        System.out.println(peer.delete(args[2]));
+                    }
                     break;
                 case "STATE":
                     System.out.println(peer.state());
                     break;
                 case "RECLAIM":
-                    if(args.length < 3){
+                    if(args.length != 3){
                         System.out.println("No number of bytes were provided. Usage: <PeerAP> RECLAIM <newSizeBytes>");
                     }else {
                         System.out.println(peer.reclaim(Integer.parseInt(args[2])));
